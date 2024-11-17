17c3404e133d83d5a59ae3625ed1f11b80ccab3c
YungCaesarr <gokuekisde@gmail.com>
2024-11-17 16:26:27 -0600

    Update redirect.lua

--- redirect.lua
+++ redirect.lua
@@ -8,19 +8,21 @@ local function checkHttpAvailable()
     end)
     return httpAvailable
 end
+    
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
+    
+if checkHttpAvailable() == false then
+  repeat task.wait(0.2) until checkHttpAvailable() == true
+end
+
+getgenv().VULTURE_REJOINING = true
+local content = game:HttpGet('https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua')
+print(content)
 
 local s2, e2 = pcall(function()
-    
-    if not game:IsLoaded() then
-      game.Loaded:Wait()
-    end
-    
-    if checkHttpAvailable() == false then
-        repeat task.wait(0.2) until checkHttpAvailable() == true
-    end
-
-    getgenv().VULTURE_REJOINING = true
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua'))()
+    loadstring(content)()
 end)
 
 if not s2 then warn('something went wrong 2'); warn(e2); end
