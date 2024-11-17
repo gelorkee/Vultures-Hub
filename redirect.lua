48edc5fdcf0956b692ec23a5a40d3dfdd2907aaa
YungCaesarr <gokuekisde@gmail.com>
2024-11-17 15:43:45 -0600

    Update redirect.lua

--- redirect.lua
+++ redirect.lua
@@ -2,8 +2,11 @@
 task.wait()
 print("[*] Redirecting...")
 local s2, e2 = pcall(function()
+    print'check 1'
     getgenv().VULTURE_REJOINING = true;
+    print'check 2'
     loadstring(game:HttpGet('https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua'))();
+    print'check 3'
 end)
 
 if not s2 then warn('something went wrong 2'); warn(e2); end
