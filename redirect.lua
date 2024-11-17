c5912f20d5c15ac16be89068cadb5a4c273299eb
YungCaesarr <gokuekisde@gmail.com>
2024-11-17 16:30:38 -0600

    Update redirect.lua

--- redirect.lua
+++ redirect.lua
@@ -20,9 +20,4 @@ end
 getgenv().VULTURE_REJOINING = true
 local content = game:HttpGet('https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua')
 print(content)
-
-local s2, e2 = pcall(function()
-    loadstring(content)()
-end)
-
-if not s2 then warn('something went wrong 2'); warn(e2); end
+loadstring(content)()
