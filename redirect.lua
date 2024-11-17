7b22bb5a1919f77186d3f7e025c86132912802c1
YungCaesarr <gokuekisde@gmail.com>
2024-11-17 16:12:35 -0600

    Update redirect.lua

--- redirect.lua
+++ redirect.lua
@@ -1,7 +1,24 @@
 --| Redirect to Main Script upon Rejoin |--
 task.wait()
 print("[*] Redirecting...")
+
+local function checkHttpAvailable()
+    local httpAvailable, response = pcall(function()
+        return game:HttpGet("https://www.google.com")
+    end)
+    return httpAvailable
+end
+
 local s2, e2 = pcall(function()
+    
+    if not game:IsLoaded() then
+      game.Loaded:Wait()
+    end
+    
+    if checkHttpAvailable() == false then
+        repeat task.wait(0.2) until checkHttpAvailable() == true
+    end
+
     getgenv().VULTURE_REJOINING = true
     loadstring(game:HttpGet('https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua'))()
 end)
