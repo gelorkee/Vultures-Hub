fa00b0d50d11d02e8b93121350914e6347cd2b53
YungCaesarr <gokuekisde@gmail.com>
2024-11-17 16:51:36 -0600

    Update redirect.lua

--- redirect.lua
+++ redirect.lua
@@ -2,22 +2,35 @@
 task.wait()
 print("[*] Redirecting...")
 
-local function checkHttpAvailable()
-    local httpAvailable, response = pcall(function()
-        return game:HttpGet("https://www.google.com")
-    end)
-    return httpAvailable
-end
-    
-if not game:IsLoaded() then
-  game.Loaded:Wait()
-end
-    
-if checkHttpAvailable() == false then
-  repeat task.wait(0.2) until checkHttpAvailable() == true
-end
+local httprequest = (syn and syn.request) or (http and http.request) or http_request or request
 
-getgenv().VULTURE_REJOINING = true
-local content = game:HttpGet('https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua')
-print(content)
-loadstring(content)()
+local s2, e2 = pcall(function()
+    
+    if not game:IsLoaded() then
+      game.Loaded:Wait()
+    end
+
+    getgenv().VULTURE_REJOINING = true
+    
+    local fetched = httprequest({
+        Url = "https://raw.githubusercontent.com/YungCaesarr/Vultures-Hub/refs/heads/main/main.lua",
+        Method = "GET",
+    })
+
+    local function sendRequest()
+        print'Req sent'
+        local sx, ex = pcall(function()
+            loadstring(fetched.Body)()
+        end)
+        return sx
+    end
+    
+    if sendRequest() == false then
+        repeat
+            task.wait()
+        until sendRequest() == true
+    end
+
+end)
+
+if not s2 then warn('[Redirect] Something went wrong. Contact an Admin.'); warn(e2); end
