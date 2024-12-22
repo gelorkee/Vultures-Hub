1444451830d26855f988bdd8c6e742855a198bab
YungCaesarr <gokuekisde@gmail.com>
2024-12-22 12:12:29 -0600

    Update emotesLoader.lua

--- emotesLoader.lua
+++ emotesLoader.lua
@@ -1,1 +1,21 @@
+        local PlayerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+        local Emotes = PlayerGui:WaitForChild("MainGUI"):WaitForChild("Game"):FindFirstChild("Emotes")
 
+        if Emotes then
+            local success = pcall(function()
+                require(game:GetService("ReplicatedStorage").Modules.EmoteModule).GeneratePage(
+                    {"headless", "zombie", "zen", "ninja", "floss", "dab", "sit"},
+                    Emotes,
+                    "Free Emotes"
+                )
+            end)
+
+            if success then
+                game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Emotes Loader";
+            Text = "Successfully unlocked all emotes.";
+            Duration = 10;
+            Button1 = "OK"
+        })
+            end
+        end
